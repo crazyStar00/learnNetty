f0bfac065e3fe8156155f91be31db079da32d129
star <653945222@qq.com>
2018-05-28 23:00:10 +0800

    hello world

---
+++ src/main/java/com/star/helloworld/client/EchoClientHandler.java
@@ -0,0 +1,31 @@
+package com.star.helloworld.client;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+	private static final Logger logger = LoggerFactory.getLogger(EchoClientHandler.class);
+
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+	}
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+		logger.info("Client received:{}",msg.toString(CharsetUtil.UTF_8));
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
+
